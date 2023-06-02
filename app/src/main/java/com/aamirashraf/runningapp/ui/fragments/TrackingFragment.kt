f11e375c067d63f7788f47b98a4bfb1c77cfa86f
Stephan Robert <aamirnasa97@gmail.com>
2023-06-03 00:16:54 +0530

    handling the maplifecycle

--- app/src/main/java/com/aamirashraf/runningapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/aamirashraf/runningapp/ui/fragments/TrackingFragment.kt
@@ -1,12 +1,63 @@
 package com.aamirashraf.runningapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.aamirashraf.runningapp.R
 import com.aamirashraf.runningapp.ui.viewmodels.MainViewModel
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TrackingFragment:Fragment(R.layout.fragment_tracking) {
     private val viewModel: MainViewModel by viewModels()
+    private lateinit var mapView:MapView
+//    private var map:GoogleMap?=null
+    private lateinit var map:GoogleMap
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView=view.findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync {
+            map=it
+        }
+
+    }
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
 }
